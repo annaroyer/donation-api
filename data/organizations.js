0f1ae596abeee8a6bf3135f2d3b178b1ec35e796
Anna Royer <anna.royer@colorado.edu>
2018-05-27 12:35:39 -0600

    add data directory with example organization data to seed database with

---
+++ data/organizations.js
@@ -0,0 +1,27 @@
+const organizations = [
+{name: "Helping Hand Mission",
+description: "Provide relief and support to needy families and victims of fire.",
+website: "http://helpinghandmission.org/",
+image: "http://helpinghandmission.org/wp-content/uploads/edd/2016/12/hhm-building-366x220.jpg",
+logo: "http://helpinghandmission.org/wp-content/uploads/2016/12/hhm-newlogo-200x90.png",
+createdAt: new Date(),
+updatedAt: new Date()},
+
+{name: "Purple Heart",
+description: "The Purple Heart is awarded to members of the armed forces of the U.S. who are wounded by an instrument of war in the hands of the enemy",
+website: "http://www.purpleheart.org/",
+image: "http://www.purpleheartpickup.org/-/media/images/charities/purpleheart/ph-01-1280x500.ashx?as=1&h=500&la=en&w=1280&hash=B24EBBC88656E7E6733A5CE1F591C58F6868C6AB",
+logo: "https://donatestuff.com/wp-content/uploads/2012/05/purple-heart1.png",
+createdAt: new Date(),
+updatedAt: new Date()},
+
+{name: "habitat for humanity",
+description: "A nonprofit organization that helps families build and improve places to call home. We believe affordable housing plays a critical role in strong and stable communities.",
+website: "https://www.habitat.org/",
+image: "https://www.habitat.org/sites/default/files/styles/16_9_default/public/2018-02/donate-goods-to-habitat-restore.jpg?itok=Bvwbdmk2",
+logo: "https://www.seeklogo.net/wp-content/uploads/2012/10/habitat-for-humanity-logo-vector.png",
+createdAt: new Date(),
+updatedAt: new Date()}
+]
+
+module.exports = organizations
