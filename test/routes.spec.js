8b1fa30996a9c843446b3c3d8f0417240535baa3
Anna Royer <anna.royer@colorado.edu>
2018-05-27 17:30:59 -0600

    update before each method in test and get test passing!!

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -5,17 +5,24 @@ const server = require('../app.js')
 
 chai.use(chaiHttp);
 
+const models = require('../models')
 const organizations = require('../data/organizations')
 
 describe('API Routes', () => {
+  beforeEach(() => {
+    return models.sequelize.sync({ force: true })
+    .then(() => models.Organization.bulkCreate(organizations))
+    .then(data => Promise.resolve(data))
+  })
+
   describe('GET /api/v1/organizations', () => {
-    it('returns all organizations', () => {
+    it('returns all organizations', function() {
       return chai.request(server)
       .get('/api/v1/organizations')
       .then(response => {
         response.should.have.status(200)
         response.should.be.json
-        response.should.deep.equal(organizations)
+        response.body.should.deep.equal(organizations)
       })
     })
   })
