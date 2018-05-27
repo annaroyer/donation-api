654144a0e289f6176681b6031697c7d2e0305394
Anna Royer <anna.royer@colorado.edu>
2018-05-27 10:16:32 -0600

    add first api test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -6,5 +6,16 @@ const server = require('../app.js')
 chai.use(chaiHttp);
 
 describe('API Routes', () => {
+  describe('GET /api/v1/organizations', () => {
+    it('returns all organizations', (done) => {
+      chai.request(server)
+      .get('/api/v1/organizations')
+      .end((error, response) => {
+        if (error) done(error)
 
+        response.should.have.status(200)
+        done()
+      })
+    })
+  })
 })
