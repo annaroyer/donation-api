bc71f5e8ecaf267fd66a528cddfcab3189d2e27f
Anna Royer <anna.royer@colorado.edu>
2018-05-27 23:30:58 -0600

    update organizations file in data

--- data/organizations.js
+++ data/organizations.js
@@ -1,6 +1,5 @@
 module.exports = [
-{id: 1,
-name: "Helping Hand Mission",
+{name: "Helping Hand Mission",
 description: "Provide relief and support to needy families and victims of fire.",
 website: "http://helpinghandmission.org/",
 image: "http://helpinghandmission.org/wp-content/uploads/edd/2016/12/hhm-building-366x220.jpg",
@@ -8,8 +7,7 @@ logo: "http://helpinghandmission.org/wp-content/uploads/2016/12/hhm-newlogo-200x
 createdAt: (new Date()).toJSON(),
 updatedAt: (new Date()).toJSON()},
 
-{id: 2,
-name: "Purple Heart",
+{name: "Purple Heart",
 description: "The Purple Heart is awarded to members of the armed forces of the U.S. who are wounded by an instrument of war in the hands of the enemy",
 website: "http://www.purpleheart.org/",
 image: "http://www.purpleheartpickup.org/-/media/images/charities/purpleheart/ph-01-1280x500.ashx?as=1&h=500&la=en&w=1280&hash=B24EBBC88656E7E6733A5CE1F591C58F6868C6AB",
@@ -17,8 +15,7 @@ logo: "https://donatestuff.com/wp-content/uploads/2012/05/purple-heart1.png",
 createdAt: (new Date()).toJSON(),
 updatedAt: (new Date()).toJSON()},
 
-{id: 3,
-name: "habitat for humanity",
+{name: "habitat for humanity",
 description: "A nonprofit organization that helps families build and improve places to call home. We believe affordable housing plays a critical role in strong and stable communities.",
 website: "https://www.habitat.org/",
 image: "https://www.habitat.org/sites/default/files/styles/16_9_default/public/2018-02/donate-goods-to-habitat-restore.jpg?itok=Bvwbdmk2",
