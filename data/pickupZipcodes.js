3550d8ed189624f99f598d6890344a439c38697e
Anna Royer <anna.royer@colorado.edu>
2018-05-29 08:14:23 -0600

    add files to data to use in seeds

---
+++ data/pickupZipcodes.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { pickup_id: 1, zipcode: "80303" },
+  { pickup_id: 1, zipcode: "80302" },
+  { pickup_id: 1, zipcode: "80304" },
+  { pickup_id: 2, zipcode: "80303" },
+  { pickup_id: 3, zipcode: "90027" },
+  { pickup_id: 3, zipcode: "90026" }
+]
---
+++ data/pickups.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { date: new Date(2018, 5, 1), organization_id: 1 },
+  { date: new Date(2018, 5, 2), organization_id: 2 },
+  { date: new Date(2018, 7, 3), organization_id: 3 }
+]
