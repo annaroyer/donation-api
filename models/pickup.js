673c0f81fe87758257573004c5d0417e968def10
Anna Royer <anna.royer@colorado.edu>
2018-05-29 08:15:38 -0600

    add pickup model with configured relationships

--- models/pickup.js
+++ models/pickup.js
@@ -1,13 +1,34 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Pickup = sequelize.define('Pickup', {
-    date: DataTypes.DATE
-  }, {});
+  const Pickup = sequelize.define('pickup', {
+    date: {
+       type: DataTypes.DATE,
+       get() {
+         let options = {weekday: 'long', year: 'numeric', month: 'long', day:'numeric'}
+         return this.getDataValue('date').toLocaleDateString('en-US', options)
+       }
+     },
+     organization_id: {
+       type: DataTypes.INTEGER,
+       references: {
+         model: sequelize.organization,
+         key: 'id'
+       }
+     }
+  }, {
+    underscored: true,
+    timestamps: false
+  });
   Pickup.associate = (models) => {
-    Pickup.belongsTo(models.Organization)
-    Pickup.hasMany(models.PickupZipcode)
-    Pickup.belongsToMany(models.Donor, { through: 'DonorPickup' })
-    Pickup.belongsToMany(models.ItemCategory, { through: 'PickupItemCategory' })
+    Pickup.belongsTo(models.organization)
+    Pickup.hasMany(models.pickup_zipcode, {as: 'zipcode'})
+    Pickup.belongsToMany(models.donor, {through: 'donor_pickup'})
+    Pickup.belongsToMany(models.item_category,
+      { through: 'pickup_item_category',
+        as: 'acceptedItems',
+        foreignKey: 'pickup_id',
+        otherKey: 'item_category_id'
+      })
   };
   return Pickup;
 };
