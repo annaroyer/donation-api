f4bffd560ec73cfef101bcf52955acaa1d8795c2
Anna Royer <anna.royer@colorado.edu>
2018-05-29 08:17:02 -0600

    add pickupsController with extensive index action full of eager loading - will refactor later

--- controllers/pickupsController.js
+++ controllers/pickupsController.js
@@ -1,8 +1,29 @@
 const db = require('../models/index')
+const Pickup = db.pickup
+const pry = require('pryjs')
 
 class PickupsController {
   static index(request, response, next){
-    response.json([{}])
+    Pickup.findAll({
+      attributes: ['date'], order: ['date'],
+      include: [
+        { association: 'zipcode',
+          where: request.query,
+          attributes: []
+        },
+        { model: db.organization,
+          attributes: ['name', 'logo']
+        },
+        { model: db.item_category,
+          as: 'acceptedItems',
+          attributes: ['name'],
+          through: {attributes: []}
+        }
+      ]
+    })
+    .then(pickups => {
+      response.json(pickups)
+    })
   }
 }
 
