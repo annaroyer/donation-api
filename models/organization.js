f5535a04700d757a3a3bbd026004663374374f6a
Anna Royer <anna.royer@colorado.edu>
2018-05-31 17:18:48 -0600

    add status and accepted_items to organization model

--- models/organization.js
+++ models/organization.js
@@ -5,8 +5,16 @@ module.exports = (sequelize, DataTypes) => {
     description: DataTypes.TEXT,
     website: DataTypes.TEXT,
     logo: DataTypes.TEXT,
-    image: DataTypes.TEXT
-  }, {});
+    image: DataTypes.TEXT,
+    accepted_items: {
+      type: DataTypes.ARRAY(DataTypes.TEXT),
+      isIn: [['clothing and accessories', 'household items', 'furniture', 'non-perishable foods']]
+    },
+    status: {
+      type: DataTypes.ENUM,
+      values: ['pending', 'accepted','rejected', 'active']
+    }
+  }, {underscored: true});
   Organization.associate = (models) => {
     Organization.hasMany(models.pickup)
     Organization.belongsToMany(models.item_category,
