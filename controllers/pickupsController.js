d90e0dae83bcf0ca7a284256fb1bd89af5f4258c
Anna Royer <anna.royer@colorado.edu>
2018-05-31 22:47:21 -0600

    update pickupsController index function to fit new database model

--- controllers/pickupsController.js
+++ controllers/pickupsController.js
@@ -1,23 +1,15 @@
 const db = require('../models/index')
 const Pickup = db.pickup
+const pry = require('pryjs')
 
 class PickupsController {
   static index(request, response, next){
     Pickup.findAll({
-      attributes: ['date'], order: ['date'],
+      attributes: ['date', ['accepted_items', 'acceptedItems']],
+      order: ['date'],
       include: [
-        { association: 'zipcode',
-          where: request.query,
-          attributes: []
-        },
-        { model: db.organization,
-          attributes: ['name', 'logo']
-        },
-        { model: db.item_category,
-          as: 'acceptedItems',
-          attributes: ['name'],
-          through: {attributes: []}
-        }
+        { association: 'zipcode', where: request.query, attributes: [] },
+        { model: db.organization, attributes: ['name', 'logo'] }
       ]
     })
     .then(pickups => response.json(pickups))
