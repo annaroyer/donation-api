fd72bd20121e29f4d328472150c333f12ae0b757
Anna Royer <anna.royer@colorado.edu>
2018-06-01 02:29:48 -0600

    add create function to organizationsControlloer

--- controllers/organizationsController.js
+++ controllers/organizationsController.js
@@ -1,12 +1,21 @@
 const Organization = require('../models').organization
 const pry = require('pryjs')
-
 class OrganizationsController {
   static index(request, response, next){
     Organization.findAll({
       attributes: ['name', 'description', 'website', 'image', 'logo']
     })
     .then(organizations => response.json(organizations))
+  }
+
+  static create(request, response, next) {
+
+    Organization.create( request.body,
+      { include: [ 'contactPerson' ] },
+      { validate: true }
+    )
+    .then(organization => response.sendStatus(200))
+    .catch(errors => response.status(400).json(errors.message))
   }
 }
 
