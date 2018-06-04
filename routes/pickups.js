aaf1e8b4e1ec0d6fd3e192f705b3fe4aac11b0d9
Anna Royer <anna.royer@colorado.edu>
2018-06-04 14:42:57 -0600

    add happy path route for posting a donor pickup

--- routes/pickups.js
+++ routes/pickups.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const PickupsController = require('../controllers/pickupsController')
+const DonorPickupsController = require('../controllers/donorPickupsController')
 
 router.get('/', PickupsController.index);
+router.post('/:id/donors', DonorPickupsController.create)
 
 module.exports = router;
