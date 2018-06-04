d3e1db470c587eb09228bf4aad675be89b82a217
Anna Royer <anna.royer@colorado.edu>
2018-06-03 22:23:08 -0600

    update organization test fixtures to pass test

--- test/fixtures/organizations.js
+++ test/fixtures/organizations.js
@@ -1,14 +1,18 @@
 module.exports = [
-  { name: "Helping Hand Mission",
+  { id: 1,
+    name: "Helping Hand Mission",
     description: "Provide relief and support to needy families and victims of fire.",
     website: "http://helpinghandmission.org/",
     image: "http://helpinghandmission.org/wp-content/uploads/edd/2016/12/hhm-building-366x220.jpg",
-    logo: "http://helpinghandmission.org/wp-content/uploads/2016/12/hhm-newlogo-200x90.png"
+    logo: "http://helpinghandmission.org/wp-content/uploads/2016/12/hhm-newlogo-200x90.png",
+    accepted_items: ["household items", "clothing and accessories"]
   },
-  { name: "Purple Heart",
+  { id: 2,
+    name: "Purple Heart",
     description: "The Purple Heart is awarded to members of the armed forces of the U.S. who are wounded by an instrument of war in the hands of the enemy",
     website: "http://www.purpleheart.org/",
     image: "http://www.purpleheartpickup.org/-/media/images/charities/purpleheart/ph-01-1280x500.ashx?as=1&h=500&la=en&w=1280&hash=B24EBBC88656E7E6733A5CE1F591C58F6868C6AB",
-    logo: "https://donatestuff.com/wp-content/uploads/2012/05/purple-heart1.png"
+    logo: "https://donatestuff.com/wp-content/uploads/2012/05/purple-heart1.png",
+    accepted_items: ["furniture", "household items"]
   }
 ]
